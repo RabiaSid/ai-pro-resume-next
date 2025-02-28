df119c9ad08c40807498f2ffb703b930e8b2d5de
Cognative <hamzvarin@outlook.com>
2025-02-28 09:09:19 +0500

    career page and apply now form

--- src/app/data.tsx
+++ src/app/data.tsx
@@ -120,3 +120,48 @@ export const FeaturesData = {
     </>
   ),
 };
+
+export const CareersBannerData = {
+  title: (
+    <>
+      Innovate Your{" "}
+      <span
+        style={{ color: "#00caa5" }}
+        className="bg-no-repeat bg-cover leading-[1.5] font-lexend font-bold"
+      >
+        Career Path
+      </span>{" "}
+      Explore Rewarding opportunities with AI Pro Resume
+    </>
+  ),
+  description: (
+    <>
+      Join our dynamic team at AI Pro Resume and be at the forefront
+      ofartificial intelligence innovation. With cutting-edge technology and a
+      collaborative, forward-thinking work atmosphere, you can shape the future
+      of resumes and job success.
+    </>
+  ),
+};
+
+export const ApplyNowBannerData = {
+  title: (
+    <>
+      Walk towards Your Future with{" "}
+      <span
+        style={{ color: "#00caa5" }}
+        className="bg-no-repeat bg-cover leading-[1.5] font-lexend font-bold"
+      >
+        AI Professional Resume
+      </span>{" "}
+      Career Opportunities
+    </>
+  ),
+  description: (
+    <>
+      Embark on a transformative career journey with our AI Pro Resume team.
+      Your next career move starts here, apply now and elevate your expertise in
+      the world of AI-driven resumes!
+    </>
+  ),
+};
