d982c03e81721c02502f84d8c2fcd327537998a8
RabiaSid <rabiasid984@gmail.com>
2025-03-03 09:44:30 +0500

    resolve conflicts v2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,19 +13,19 @@ import Clients from "@/components/clients/Clients";
 import Reviews from "@/components/reviews/Reviews";
 import Features from "@/components/features/Features";
 import Services from "@/components/services/Services";
+import OnScrollAnime from '@/components/onScrollAnime/OnScrollAnime';
 
 export default function Page() {
   return (
     <>
-      <div className="container md:px-4 mx-auto">
-        <MainBanner {...BannerData} />
-        <ResumeTemplateSlider {...ResumeTemplateData} />
-        <Services />
-        <Features {...FeaturesData} />
-        <OurFaqs />
-        <Clients {...ClientData} />
-        <Reviews {...ReviewData} />
-      </div>
+      <MainBanner {...BannerData} />
+      <ResumeTemplateSlider {...ResumeTemplateData} />
+      <OnScrollAnime />
+      <Services />
+      <Features {...FeaturesData} />
+      <OurFaqs />
+      <Clients {...ClientData} />
+      <Reviews {...ReviewData} />
     </>
   );
 }
