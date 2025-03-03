9391ed5e5d57e0e84e692abcab202fd2bbaaab4c
Cognative <hamzvarin@outlook.com>
2025-03-03 15:47:22 +0500

    services page

--- src/app/data.tsx
+++ src/app/data.tsx
@@ -181,3 +181,36 @@ export const ContactUsBannerData = {
   ),
   description: <></>,
 };
+
+export const ServiceBannerData = {
+  title: (
+    <>
+      <span
+        // style={{ color: "#00caa5" }}
+        className="bg-no-repeat bg-cover leading-[1.5] font-lexend text-xl font-bold"
+      >
+        Craft Your Success Story:
+      </span>
+      <br />
+      Let’s Perfect{" "}
+      <span
+        style={{ color: "#00caa5" }}
+        className="bg-no-repeat bg-cover leading-[1.5] font-lexend font-bold"
+      >
+        Your Resume
+      </span>
+      <br />& Cover Letter Together
+    </>
+  ),
+  description: (
+    <>
+      Writing a professional cover letter and resume can be a hectic task, and
+      it will not guarantee you will pass the ATS checkers. AI Pro Resume is
+      here to cater to all your needs by offering top-notch professional cover
+      letter writing service and resume writing services. We also provide cover
+      letter review and professional resume review services to assist you in
+      creating out-class resumes and cover letters. Our experts analyze your
+      documents and provide suggestions based on your job positions and career.
+    </>
+  ),
+};
